1e2ca06750941014911ba7d54a8342addbce9c9e
ruslank <shaman2804.rk@gmail.com>
2023-03-08 13:28:16 -0600

    add Identity and customize it

--- RazorPagesPizza/Program.cs
+++ RazorPagesPizza/Program.cs
@@ -1,5 +1,16 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesPizza.Areas.Identity.Data;
 
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("RazorPagesPizzaAuthConnection");
+builder.Services.AddDbContext<RazorPagesPizzaAuth>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDbContext<RazorPagesPizzaAuth>(options =>
+    options.UseSqlServer(connectionString!));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<RazorPagesPizzaAuth>();
 // Add services to the container.
 builder.Services.AddRazorPages();
 
@@ -17,7 +28,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
